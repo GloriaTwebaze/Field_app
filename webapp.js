db932c1f2951118260fcba9b159ae7012f00480d
twebaze gloria <twebazegloria909@gmail.com>
2021-03-18 02:26:25 +0100

    The garbage app

---
+++ webapp.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+
+
+app.get('/', (req, res) =>{
+    res.render('view')
+})
+
+app.listen(2008, () => console.log('server started at 2008'))
